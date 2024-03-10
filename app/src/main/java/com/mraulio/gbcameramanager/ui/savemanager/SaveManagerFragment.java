5fa25580cbb8ae0c66720d1202f839e389d2e049
Raul M <raulmvidal87@gmail.com>
2024-03-10 12:30:24 +0100

    Improved Save Manager

--- app/src/main/java/com/mraulio/gbcameramanager/ui/savemanager/SaveManagerFragment.java
+++ app/src/main/java/com/mraulio/gbcameramanager/ui/savemanager/SaveManagerFragment.java
@@ -1,5 +1,8 @@
 package com.mraulio.gbcameramanager.ui.savemanager;
 
+import static com.mraulio.gbcameramanager.utils.Utils.gbcImagesList;
+import static com.mraulio.gbcameramanager.utils.Utils.retrieveTags;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
@@ -22,13 +25,16 @@ import android.widget.ListView;
 
 import com.mraulio.gbcameramanager.MainActivity;
 import com.mraulio.gbcameramanager.R;
+import com.mraulio.gbcameramanager.db.ImageDao;
+import com.mraulio.gbcameramanager.db.ImageDataDao;
 import com.mraulio.gbcameramanager.gameboycameralib.codecs.ImageCodec;
 import com.mraulio.gbcameramanager.gameboycameralib.constants.IndexedPalette;
 import com.mraulio.gbcameramanager.gameboycameralib.saveExtractor.Extractor;
 import com.mraulio.gbcameramanager.gameboycameralib.saveExtractor.SaveImageExtractor;
 import com.mraulio.gbcameramanager.model.GbcImage;
+import com.mraulio.gbcameramanager.model.ImageData;
 import com.mraulio.gbcameramanager.ui.gallery.CustomGridViewAdapterImage;
-import com.mraulio.gbcameramanager.ui.gallery.GalleryFragment;
+import com.mraulio.gbcameramanager.ui.importFile.ImportFragment;
 import com.mraulio.gbcameramanager.utils.Utils;
 
 import java.io.File;
@@ -39,26 +45,27 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.Locale;
 
 public class SaveManagerFragment extends Fragment {
     private ListView listView;
     private GridView gridviewSaves;
     private List<String> fileList;
     private String saveName;
-    private Button btnDelete;
+    private Button btnDelete, btnAdd;
     private AlertDialog loadingDialog;
     private File selectedFile;
     private CustomGridViewAdapterImage gridAdapter;
     private ArrayAdapter<String> listViewAdapter;
+    private int numImagesAdded = 0;
 
     List<GbcImage> listActiveImages = new ArrayList<>();
     List<Bitmap> listActiveBitmaps = new ArrayList<>();
-    List<GbcImage> listDeletedImages;
-    List<Bitmap> listDeletedBitmaps;
-    List<Bitmap> listDeletedBitmapsRedStroke;
-    List<GbcImage> finalListImages;
-    List<Bitmap> finalListBitmaps;
+    List<GbcImage> listDeletedImages = new ArrayList<>();
+    List<Bitmap> listDeletedBitmaps = new ArrayList<>();
+    List<Bitmap> listDeletedBitmapsRedStroke = new ArrayList<>();
+    List<GbcImage> finalListImages = new ArrayList<>();
+    List<Bitmap> finalListBitmaps = new ArrayList<>();
     GbcImage lastSeenImage;
     Bitmap lastSeenBitmap;
     List<Bitmap> extractedImagesBitmaps = new ArrayList<>();
@@ -77,6 +84,7 @@ public class SaveManagerFragment extends Fragment {
         listView = view.findViewById(R.id.listView);
         gridviewSaves = view.findViewById(R.id.gridViewSaves);
         btnDelete = view.findViewById(R.id.btnDelete);
+        btnAdd = view.findViewById(R.id.btnAdd);
         loadingDialog = Utils.loadingDialog(getContext());
         MainActivity.current_fragment = MainActivity.CURRENT_FRAGMENT.SAVE_MANAGER;
 
@@ -99,9 +107,87 @@ public class SaveManagerFragment extends Fragment {
                 createDialog();
             }
         });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                List<GbcImage> newGbcImages = new ArrayList<>();
+                List<ImageData> newImageDatas = new ArrayList<>();
+                for (int i = 0; i < extractedImagesList.size(); i++) {
+                    GbcImage gbcImage = extractedImagesList.get(i);
+                    boolean alreadyAdded = false;
+                    //If the image already exists (by the hash) it doesn't add it. Same if it's already added
+                    for (GbcImage image : Utils.gbcImagesList) {
+                        if (image.getHashCode().toLowerCase(Locale.ROOT).equals(gbcImage.getHashCode())) {
+                            alreadyAdded = true;
+                            break;
+                        }
+                    }
+                    if (!alreadyAdded) {
+                        GbcImage.numImages++;
+                        numImagesAdded++;
+                        ImageData imageData = new ImageData();
+                        imageData.setImageId(gbcImage.getHashCode());
+                        imageData.setData(gbcImage.getImageBytes());
+                        newImageDatas.add(imageData);
+                        Utils.gbcImagesList.add(gbcImage);
+                        newGbcImages.add(gbcImage);
+                        Utils.imageBitmapCache.put(gbcImage.getHashCode(), extractedImagesBitmaps.get(i));
+                    }
+                }
+                if (newGbcImages.size() > 0) {
+                    new SaveImageAsyncTask(newGbcImages, newImageDatas).execute();
+                    retrieveTags(gbcImagesList);
+                } else {
+                    Utils.toast(getContext(), getString(R.string.no_new_images));
+                }
+
+            }
+        });
+
         return view;
     }
 
+    //Refactor this, already in Import and USB Serial Fragment!!
+    private class SaveImageAsyncTask extends AsyncTask<Void, Void, Void> {
+        List<GbcImage> gbcImagesList;
+        List<ImageData> imageDataList;
+        public SaveImageAsyncTask(List<GbcImage> gbcImagesList, List<ImageData> imageDataList) {
+            this.gbcImagesList = gbcImagesList;
+            this.imageDataList = imageDataList;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            ImageDao imageDao = MainActivity.db.imageDao();
+            ImageDataDao imageDataDao = MainActivity.db.imageDataDao();
+            //Need to insert first the gbcImage because of the Foreign Key
+            try {
+
+                for (GbcImage gbcImage : gbcImagesList) {
+                    imageDao.insert(gbcImage);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                for (ImageData imageData : imageDataList) {
+                    imageDataDao.insert(imageData);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Utils.toast(getContext(), getString(R.string.images_added) + numImagesAdded);
+        }
+    }
 
     private void loadFileNames() {
         try {
@@ -122,7 +208,6 @@ public class SaveManagerFragment extends Fragment {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     private class loadDataTask extends AsyncTask<Void, Void, Void> {
@@ -143,7 +228,14 @@ public class SaveManagerFragment extends Fragment {
             super.onPostExecute(aVoid);
 
             loadingDialog.dismiss();
-            showImages();
+            if (lastSeenImage != null) {
+                showImages(gridviewSaves, gridAdapter);
+                btnAdd.setEnabled(true);
+
+            } else {
+                gridviewSaves.setAdapter(null);
+                btnAdd.setEnabled(false);
+            }
             btnDelete.setEnabled(true);
         }
     }
@@ -157,7 +249,7 @@ public class SaveManagerFragment extends Fragment {
             if (selectedFile.length() / 1024 == 128) {
                 List<byte[]> listExtractedImageBytes = new ArrayList<>();
 
-                listExtractedImageBytes = extractor.extractBytes(selectedFile,saveBank);
+                listExtractedImageBytes = extractor.extractBytes(selectedFile, saveBank);
                 int nameIndex = 1;
                 String fileName = selectedFile.getName();
                 for (byte[] imageBytes : listExtractedImageBytes) {
@@ -175,7 +267,7 @@ public class SaveManagerFragment extends Fragment {
                     String hashHex = Utils.bytesToHex(hash);
                     gbcImage.setHashCode(hashHex);
                     ImageCodec imageCodec = new ImageCodec(128, 112, false);
-                    Bitmap image = imageCodec.decodeWithPalette(Utils.hashPalettes.get(gbcImage.getPaletteId()).getPaletteColorsInt(), Utils.hashPalettes.get(gbcImage.getFramePaletteId()).getPaletteColorsInt(),imageBytes, false,false,false);
+                    Bitmap image = imageCodec.decodeWithPalette(Utils.hashPalettes.get(gbcImage.getPaletteId()).getPaletteColorsInt(), Utils.hashPalettes.get(gbcImage.getFramePaletteId()).getPaletteColorsInt(), imageBytes, false, false, false);
                     if (image.getHeight() == 112 && image.getWidth() == 128) {
                         //I need to use copy because if not it's inmutable bitmap
                         Bitmap framed = Utils.hashFrames.get((gbcImage.getFrameId())).getFrameBitmap().copy(Bitmap.Config.ARGB_8888, true);
@@ -210,7 +302,14 @@ public class SaveManagerFragment extends Fragment {
                     listDeletedBitmapsRedStroke.add(copiedBitmap);
                 }
             } else {
+                listActiveImages.clear();
+                listActiveBitmaps.clear();
+                lastSeenImage = null;
+                lastSeenBitmap = null;
+                listDeletedImages.clear();
 
+                listDeletedBitmaps.clear();
+                listDeletedBitmapsRedStroke.clear();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -220,7 +319,7 @@ public class SaveManagerFragment extends Fragment {
     }
 
     //Refactor
-    private void showImages() {
+    private void showImages(GridView gridView, CustomGridViewAdapterImage gridAdapterSI) {
 
         finalListImages = new ArrayList<>(listActiveImages);
         finalListBitmaps = new ArrayList<>(listActiveBitmaps);
@@ -231,8 +330,8 @@ public class SaveManagerFragment extends Fragment {
         finalListBitmaps.addAll(listDeletedBitmaps);
         bitmapsAdapterList.addAll(listDeletedBitmapsRedStroke);
 
-        gridviewSaves.setAdapter((new CustomGridViewAdapterImage(getContext(), R.layout.row_items, finalListImages, bitmapsAdapterList, true, true, false, null)));
-        gridAdapter = (CustomGridViewAdapterImage) gridviewSaves.getAdapter();
+        gridView.setAdapter((new CustomGridViewAdapterImage(getContext(), R.layout.row_items, finalListImages, bitmapsAdapterList, true, true, false, null)));
+        gridAdapterSI = (CustomGridViewAdapterImage) gridviewSaves.getAdapter();
     }
 
     private void createDialog() {
@@ -242,7 +341,11 @@ public class SaveManagerFragment extends Fragment {
         GridView grid = new GridView(getContext());
         grid.setNumColumns(4);
         grid.setPadding(30, 10, 30, 10);
-        grid.setAdapter(new CustomGridViewAdapterImage(getContext(), R.layout.row_items, finalListImages, bitmapsAdapterList, true, false, false, null));
+        if (lastSeenImage != null) {
+            showImages(grid, gridAdapter);
+        } else {
+            grid.setAdapter(null);
+        }
 
         builder.setView(grid);
 
